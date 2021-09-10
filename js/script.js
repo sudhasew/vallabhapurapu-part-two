2b12d168b6874486d6941b4435a2060912b84410
sudhasew <sudha.sew@gmail.com>
2021-09-09 20:50:54 -0400

    Modified the script as per specifications

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
-let Pet = prompt ("Name your pet");
-document.write(Action);
-let Energy = 0;
-let Happiness = 0;
-document.write(Energy);
-document.write(Happiness);
+let pet = prompt ("Pet's name");
+document.write(action);
+let energy = 0;
+let happiness = 0;
+document.write(energy);
+document.write(happiness);
 for (i=1; i<7; i++){
-var Action = prompt ("feed, pet, or walk?");
-   if (Action === "feed") {
-Energy = (Energy + 2);
-   } else if (Action === "pet") {
-Happiness = (Happiness + 1);
-   } else if (Action === "walk")
+var action = prompt ("feed, pet or walk?");
+   if (action === "feed") {
+energy = (energy + 2);
+   } else if (action === "pet") {
+happiness = (happiness + 1);
+   } else if (action === "walk")
   {
-  if(Energy === 0)
+  if(energy === 0)
     {
-      alert("Not enough to enjoy a walk");
+      alert("Not enough energy to enjoy a walk.");
     }
-  else if(Energy>0)
+  else if(energy>0)
     {
-      Happiness = ( Happiness + 2); 
-      Energy = (Energy - 1);
+      happiness = ( happiness + 2); 
+      energy = (energy - 1);
    } 
    
  } 
 
-}; 
-console.log( Pet+" has " + Happiness + " happiness and " + Energy + " energy"); 
+};
+console.log("Pet's Name : "+pet);
+console.log("Pet's Happiness : "+happiness+" and Energy Level : "+energy);
+console.log( pet+" has " + happiness + " happiness and " + energy + " energy."); 
